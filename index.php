5383927d1e87f0d88cdba9f3c74730fb59cb9d60
Sergey.Berezin <skazka064@mail.ru>
2019-09-06 11:57:39 +0400

    9

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 $text= "стеклянный оловянный деревянный стальной обычный солнечный";
 $pieces = explode(" ", $text);
-echo count($pieces);
+
 for ($i=0; $i<count($pieces);$i++) {
-    preg_match('/(.*)/', $pieces[$i], $matches);
-$arr[$i] = $matches[$i];
+    preg_match('/(.*)[ный]/', $pieces[$i], $matches);
+
+    $arr1[]=$matches;
 
 }
 
+echo count($arr1);
+
+
+
 echo "<pre>";
-print_r($arr);
+print_r($arr1);
 ?>
