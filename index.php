534f3160df7444186a756e4d6dffcc3abc30b092
Sergey.Berezin <skazka064@mail.ru>
2019-09-09 13:55:49 +0400

    11

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
-$text= "стеклянный оловянный деревянный стальной обычный солнечный солнечная сулугуни";
+$text= "стеклянный оловянный деревянный стальной обычный солнечный солнечная сулугуни солнечная солнечное";
 $pieces = explode(" ", $text);
 
 for ($i=0; $i<count($pieces);$i++) {
-    preg_match('/(ный$)|(ное$)|(ная$)/', $pieces[$i], $matches);
-
+    preg_match('/.*(ный$)|.*(ное$)|.*(ная$)/', $pieces[$i], $matches);
     $arr1[]=$matches;
 
 }
 
+for ($i=0; $i<count($arr1);$i++){
+    $rez[]=$arr1[$i][0];
+}
 
+$rez= array_unique($rez);
 
 echo "<pre>";
-print_r($arr1);
+print_r($rez);
 ?>
