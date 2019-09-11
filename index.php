1f72f276cae5580fdce8581b90dffd6b79135719
Sergey.Berezin <skazka064@mail.ru>
2019-09-11 13:57:33 +0400

    22

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
+ini_set('error_reporting', E_ALL);
 $text= "стеклянный оловянный деревянный стальной обычный солнечный солнечная сулугуни солнечная солнечное";
 $pieces = explode(" ", $text);
 
 for ($i=0; $i<count($pieces);$i++) {
-    preg_match('/.*(ный$)|.*(ное$)|.*(ная$)/', $pieces[$i], $matches);
+    preg_match('/.*(ный$)|.*(ное$)|.*(ная$)|.*(ной$)/', $pieces[$i], $matches);
     $arr1[]=$matches;
 
 }
 
 for ($i=0; $i<count($arr1);$i++){
+
+    if (!isset($arr1[$i][0])){
+        $arr1[$i][0]=null;
+    }
     $rez[]=$arr1[$i][0];
 }
 
 $rez= array_unique($rez);
-
+$new_arr = array_diff($rez, array(''));
 echo "<pre>";
-print_r($rez);
+print_r($new_arr);
 ?>
