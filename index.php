2c4b9bfd5c6b42b06cca4bae733f3c6eca97ba51
Sergey.Berezin <skazka064@mail.ru>
2019-10-01 10:22:00 +0400

    2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set('error_reporting', E_ALL);
  class Word
  {
      public $text;
-     public $pieces;
+
 
      public function __construct($param)
      {
@@ -13,16 +13,19 @@ ini_set('error_reporting', E_ALL);
 
      }
 
-     public function pie(){
-         $pieces = explode(" ", $this->text);
-         return $pieces;
-     }
+     public function isAdjactive(){
 
+         if(preg_match('/.*(ный$)|.*(ное$)|.*(ная$)|.*(ной$)/', $this->text)) return true;
+         return false;
+     }
 
 
 
  }
 
+$obj= new Word("красивый");
 
+ echo"<pre>";
+ echo var_dump($obj);
 
 ?>
